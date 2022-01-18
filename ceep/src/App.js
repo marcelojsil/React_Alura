c7acb68ed04480e2dfd0d98ac8fdf671d8004ebd
Marthen88 <dev.marcelojs@gmail.com>
2022-01-17 23:46:12 -0300

    início

---
+++ ceep/src/App.js
@@ -0,0 +1,14 @@
+import './App.css';
+
+function App() {
+  return (
+    <>
+      <h1>MEU PRIMEIRO REACT</h1>
+      <div>
+        <p>Subtítulo</p>
+      </div>
+    </>
+  );
+}
+
+export default App;
